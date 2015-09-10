b949ad6a6aa8924839ae03370560d03fd8086479
MrFabkirox <fabrice.esope@gmail.com>
2015-09-10 12:08:14 +0100

    First real online stuff

--- resources/views/menu.blade.php
+++ resources/views/menu.blade.php
@@ -15,8 +15,8 @@
 	<h3>Interaction Tiger Space</h3>
 
 <ul style="list-style-type:none;">
-	<li><a href="http://interactiontigerspace.herokuapp.com/welcome">welcome</a></li>	
-	<li><a href="http://interactiontigerspace.herokuapp.com/">page1</a></li>
+	<li><a href="http://interactiontigerspace.herokuapp.com/">welcome</a></li>	
+	<li><a href="http://interactiontigerspace.herokuapp.com/page1">page1</a></li>
 	<li><a href="http://interactiontigerspace.herokuapp.com/page2">page2</a></li>
 	<li><a href="http://interactiontigerspace.herokuapp.com/page3">page3</a></li>
 	<li><a href="http://interactiontigerspace.herokuapp.com/page4">page4</a></li>
